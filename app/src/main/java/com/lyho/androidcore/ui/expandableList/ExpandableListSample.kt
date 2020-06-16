dc71b008dc8cf36fee8a5b7663cd79883214844b
Ly Ho Van <lyhv@tech.est-rouge.com>
2020-06-16 14:41:17 +0700

    add base motion layout

---
+++ app/src/main/java/com/lyho/androidcore/ui/expandableList/ExpandableListSample.kt
@@ -0,0 +1,10 @@
+package com.lyho.androidcore.ui.expandableList
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.MergeAdapter
+
+internal class ExpandableListSample : AppCompatActivity() {
+    private fun init() {
+        val mergeAdapter: MergeAdapter
+    }
+}
